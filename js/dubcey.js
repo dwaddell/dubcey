4986e788a58441e663c581490172a42fe5887fac
Drew Waddell <drew.waddell@gmail.com>
2013-09-03 20:23:56 -0400

    Moved init code

--- js/dubcey.js
+++ js/dubcey.js
@@ -1,6 +1,17 @@
 // ACW 9/3/13 #1 The beginning, this file will start it all
-
 $(document).ready( function ()
 {
-	alert('testing');
+	// ACW 9/3/13 Moved the initialize code to the .ready function
+	Parse.initialize("8SyaVoZSUSwjTpFrq8i4o8otPRbWMLXZ7PtEAgMR", "S7SGiM2ApbQpRkVZn7ixXoMjLkoM9y6SPGo8pdvl");
+	
+	var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+      testObject.save({foo: "bar"}, {
+      success: function(object) {
+        $(".success").show();
+      },
+      error: function(model, error) {
+        $(".error").show();
+      }
+    });	
 });
