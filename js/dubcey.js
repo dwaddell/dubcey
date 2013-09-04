2a5f139b24be0939a66e116d459e44fbaf790404
Drew Waddell <drew.waddell@gmail.com>
2013-09-03 20:27:54 -0400

    Numerous changes to test Parse.com user object

--- js/dubcey.js
+++ js/dubcey.js
@@ -4,14 +4,26 @@ $(document).ready( function ()
 	// ACW 9/3/13 Moved the initialize code to the .ready function
 	Parse.initialize("8SyaVoZSUSwjTpFrq8i4o8otPRbWMLXZ7PtEAgMR", "S7SGiM2ApbQpRkVZn7ixXoMjLkoM9y6SPGo8pdvl");
 	
-	var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-      testObject.save({foo: "bar"}, {
-      success: function(object) {
-        $(".success").show();
-      },
-      error: function(model, error) {
-        $(".error").show();
-      }
-    });	
+	var oCurrentUser = Parse.User.current();
+	if (oCurrentUser) 
+	{
+    	// We have a user
+    	alert("user is logged in");
+	} 
+	else 
+	{
+	    // Show Login page
+		alert("show login");
+	}
+	
+	// var TestObject = Parse.Object.extend("TestObject");
+//     var testObject = new TestObject();
+//       testObject.save({foo: "bar"}, {
+//       success: function(object) {
+//         $(".success").show();
+//       },
+//       error: function(model, error) {
+//         $(".error").show();
+//       }
+//     });	
 });
