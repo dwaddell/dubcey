e1144f1c1d7fa06589d7a3be0ab820525adf4516
Drew Waddell <drew.waddell@gmail.com>
2013-11-01 13:33:06 -0400

    Trying to add a login page

--- js/dubcey.js
+++ js/dubcey.js
@@ -3,28 +3,92 @@ $(document).ready( function ()
 {
 	// ACW 9/3/13 Moved the initialize code to the .ready function
 	Parse.initialize("8SyaVoZSUSwjTpFrq8i4o8otPRbWMLXZ7PtEAgMR", "S7SGiM2ApbQpRkVZn7ixXoMjLkoM9y6SPGo8pdvl");
+
 	
-	var oCurrentUser = Parse.User.current();
-	if (oCurrentUser) 
-	{
-    	// We have a user
-    	alert("user is logged in");
-	} 
-	else 
-	{
-	    // Show Login page
-		//alert("Hello World");
-		new LogInView();
-	}
-	
-	// var TestObject = Parse.Object.extend("TestObject");
-//     var testObject = new TestObject();
-//       testObject.save({foo: "bar"}, {
-//       success: function(object) {
-//         $(".success").show();
-//       },
-//       error: function(model, error) {
-//         $(".error").show();
-//       }
-//     });	
+ var LogInView = Parse.View.extend({
+    events: {
+      "submit form.login-form": "logIn",
+      "submit form.signup-form": "signUp"
+    },
+
+    el: ".content",
+    
+    initialize: function() {
+      _.bindAll(this, "logIn", "signUp");
+      this.render();
+    },
+
+    logIn: function(e) {
+      var self = this;
+      var username = this.$("#login-username").val();
+      var password = this.$("#login-password").val();
+      
+      Parse.User.logIn(username, password, {
+        success: function(user) {
+          new ManageTodosView();
+          self.undelegateEvents();
+          delete self;
+        },
+
+        error: function(user, error) {
+          self.$(".login-form .error").html("Invalid username or password. Please try again.").show();
+          this.$(".login-form button").removeAttr("disabled");
+        }
+      });
+
+      this.$(".login-form button").attr("disabled", "disabled");
+
+      return false;
+    },
+
+    signUp: function(e) {
+      var self = this;
+      var username = this.$("#signup-username").val();
+      var password = this.$("#signup-password").val();
+      
+      Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
+        success: function(user) {
+          new ManageTodosView();
+          self.undelegateEvents();
+          delete self;
+        },
+
+        error: function(user, error) {
+          self.$(".signup-form .error").html(error.message).show();
+          this.$(".signup-form button").removeAttr("disabled");
+        }
+      });
+
+      this.$(".signup-form button").attr("disabled", "disabled");
+
+      return false;
+    },
+
+    render: function() {
+      this.$el.html(_.template($("#login-template").html()));
+      this.delegateEvents();
+    }
+  });
+
+
+  var AppView = Parse.View.extend({
+    // Instead of generating a new element, bind to the existing skeleton of
+    // the App already present in the HTML.
+    el: $("#dubcey"),
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      if (Parse.User.current()) {
+      	alert("we have a user!"); //  new ManageTodosView();
+      } else {
+        new LogInView();
+      }
+    }
+  });
+  
+   new AppView;
+   Parse.history.start();
 });
