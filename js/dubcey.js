8a31f0403cf10303f18c8110cf3ef3929cd7e551
Drew Waddell <drew.waddell@gmail.com>
2013-11-07 19:15:19 -0500

    Spacing

--- js/dubcey.js
+++ js/dubcey.js
@@ -38,128 +38,132 @@ $(document).ready( function ()
   });
 
  var SignUpView = Parse.View.extend({
- 	events:{
- 		"submit form.signup-form": "signUp"
- 		},
+  events        : {
+ 		                "submit form.signup-form": "signUp"
+ 		              },
  		
- 		el: $(".content"),
+ 	el            : $(".content"),
  		
- 		initialize: function() {
-      	  _.bindAll(this, "signUp");
-          this.render();
-    	},
+ 	initialize    : function() 
+ 	                {
+                    _.bindAll(this, "signUp");
+                    this.render();
+                  },
  		
- 		signUp: function(e) {
- 			console.log("We want to sign up!");	
- 			var self = this;
-      		var username = this.$("#signup-username").val();
-      		var password = this.$("#signup-password").val();
-      
-      		Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
-        		success: function(user) {
-          			new WelcomeView();
-          			self.undelegateEvents();
-          			delete self;
-        			},
+ 	signUp        : function(e) 
+ 	                {
+                    var self = this;
+                    var username = this.$("#signup-username").val();
+                    var password = this.$("#signup-password").val();
 
-        		error: function(user, error) {
-          			self.$(".signup-form .error").html(error.message).show();
-          			this.$(".signup-form button").removeAttr("disabled");
-        			}
-      		});
+                    Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
+                      success: function(user) {
+                          new WelcomeView();
+                          self.undelegateEvents();
+                          delete self;
+                        },
 
-      		this.$(".signup-form button").attr("disabled", "disabled");
+                      error: function(user, error) {
+                          self.$(".signup-form .error").html(error.message).show();
+                          this.$(".signup-form button").removeAttr("disabled");
+                        }
+                    });
 
- 		},
+                    this.$(".signup-form button").attr("disabled", "disabled");
+                  },
  			
- 	  	render: function() {
- 	  		var sTemplate;
- 	  		
- 	  		sTemplate='<header id="header"></header>'+
-  				'<div class="signup">'+	
-					'<form class="signup-form">'+
-						'<h2>Sign Up</h2>'+
-						'<div class="error" style="display:none"></div>'+
-						'<input type="text" id="signup-username" placeholder="Username" />'+
-	  					'<input type="password" id="signup-password" placeholder="Create a Password" />'+
-	  					'<button>Sign Up</button>'+
-					'</form>'+
-  				'</div>';
-  
-      		this.$el.html(_.template(sTemplate));
-      		this.$el.find('button').button();
-      		this.delegateEvents();
-    	}
+  render        : function() 
+                  {
+                    var sTemplate;
+      
+                    sTemplate='<header id="header"></header>'+
+                      '<div class="signup">'+	
+                      '<form class="signup-form">'+
+                        '<h2>Sign Up</h2>'+
+                        '<div class="error" style="display:none"></div>'+
+                        '<input type="text" id="signup-username" placeholder="Username" />'+
+                          '<input type="password" id="signup-password" placeholder="Create a Password" />'+
+                          '<button>Sign Up</button>'+
+                      '</form>'+
+                      '</div>';
+
+                    this.$el.html(_.template(sTemplate));
+                    this.$el.find('button').button();
+                    this.delegateEvents();
+    	            }
  });
  				
  var LogInView = Parse.View.extend({
-    events: {
-      "submit form.login-form": "logIn",
-      "click button.signup-button": "signUp"
-    },
+    events      : {
+                    "submit form.login-form": "logIn",
+                    "click button.signup-button": "signUp"
+                  },
 
-    el: $(".content"),
+    el          : $(".content"),
     
-    initialize: function() {
-      _.bindAll(this, "logIn", "signUp");
-      this.render();
-    },
+    initialize  : function() 
+                  {
+                    _.bindAll(this, "logIn", "signUp");
+                    this.render();
+                  },
 
-    logIn: function(e) {
-      var self = this;
-      var username = this.$("#login-username").val();
-      var password = this.$("#login-password").val();
+    logIn       : function(e) 
+                  {
+                    var self = this;
+                    var username = this.$("#login-username").val();
+                    var password = this.$("#login-password").val();
       
-      Parse.User.logIn(username, password, {
-        success: function(user) {
-          // new WelcomeView();
-          self.undelegateEvents();
-          delete self;
-          oRouter.navigate('', true);
-        },
+                    Parse.User.logIn(username, password, {
+                      success: function(user) {
+                        // new WelcomeView();
+                        self.undelegateEvents();
+                        delete self;
+                        oRouter.navigate('', true);
+                      },
 
-        error: function(user, error) {
-          self.$(".login-form .error").html("Invalid username or password. Please try again.").show();
-          this.$(".login-form button").removeAttr("disabled");
-        }
-      });
+                      error: function(user, error) {
+                        self.$(".login-form .error").html("Invalid username or password. Please try again.").show();
+                        this.$(".login-form button").removeAttr("disabled");
+                      }
+                    });
 
-      this.$(".login-form button").attr("disabled", "disabled");
+                    this.$(".login-form button").attr("disabled", "disabled");
 
-      return false;
-    },
+                    return false;
+                  },
 
-    signUp: function(e) {
-  		this.undelegateEvents();
-      delete this;
-      
-      oRouter.navigate('signUp', true); //new SignUpView();
+    signUp      : function(e) 
+                  {
+                    this.undelegateEvents();
+                    delete this;
     
-      return false;
-    },
+                    oRouter.navigate('signUp', true); //new SignUpView();
+  
+                    return false;
+                  },
 
-    render: function() {
+    render      : function() 
+                  {
+                    var sTemplate;
     
-      	var sTemplate;
-      
-      	sTemplate='<header id="header"></header>'+
-  			'<div class="login">'+
-				'<form class="login-form">'+
-	  				'<h2>Log In</h2>'+
-	  				'<div class="error" style="display:none"></div>'+
-	  				'<input type="text" id="login-username" placeholder="Username" />'+
-	 				'<input type="password" id="login-password" placeholder="Password" />'+
-	  				'<button>Log In</button>'+
-				'</form>'+
-				'<div class="signup">'+
-  					'<button class="signup-button">Sign Up</button>'+
-  				'</div>'+
-  			'</div>';
+                    sTemplate='<header id="header"></header>'+
+                    '<div class="login">'+
+                    '<form class="login-form">'+
+                        '<h2>Log In</h2>'+
+                        '<div class="error" style="display:none"></div>'+
+                        '<input type="text" id="login-username" placeholder="Username" />'+
+                      '<input type="password" id="login-password" placeholder="Password" />'+
+                        '<button>Log In</button>'+
+                    '</form>'+
+                    '<div class="signup">'+
+                        '<button class="signup-button">Sign Up</button>'+
+                      '</div>'+
+                    '</div>';
 
-      this.$el.html(_.template(sTemplate));
-      this.$el.find('button').button();
-      this.delegateEvents();
-    }
+                  this.$el.html(_.template(sTemplate));
+                  this.$el.find('button').button();
+                  this.delegateEvents();
+                }
   });
 
 
@@ -199,21 +203,18 @@ $(document).ready( function ()
   var AppView = Parse.View.extend({
     // Instead of generating a new element, bind to the existing skeleton of
     // the App already present in the HTML.
-    el: $("#dubcey"),
+    el          : $("#dubcey"),
 
-    initialize: function() {
-      this.render();
-    },
+    initialize  : function() 
+                  {
+                    this.render();
+                  },
 
-    render    : function() 
-                {
-                  oRouter.navigate('', true);
-                }
+    render      : function() 
+                  {
+                   oRouter.navigate('', true);
+                 }
   });
-  
-  
-  
-
 
   new AppView;
   Parse.history.start();
